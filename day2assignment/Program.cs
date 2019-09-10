767926e50cb740addcbe219871e8da1467e364d2
mmosier-web <mmosier5@gmail.com>
2019-09-10 10:38:16 -0400

    first commit

---
+++ day2assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace day2assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area of a square calculator");
+
+            Console.WriteLine("Enter the length");
+
+            int x = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Enter the width");
+
+            int y=  Convert.ToInt32(Console.ReadLine()); ;
+
+            int area = x * y;
+
+            Console.WriteLine("area = " + area + " units squared");
+        }
+    }
+}
